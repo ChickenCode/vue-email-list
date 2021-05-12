fa8ae232a2ad5a22446de900f3ab8a43658cd2fe
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-05-12 13:32:09 -0400

    per gli amanti dell'hardcoded

--- myScript.js
+++ myScript.js
@@ -10,8 +10,7 @@ const app = new Vue ({
         for (i = 0; i < this.cicleTimes; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then(resp => {
-                let randomEmail = resp.data.response
-                this.emailList.push(randomEmail)
+                this.emailList.push(resp.data.response)
             })
         } 
     }
