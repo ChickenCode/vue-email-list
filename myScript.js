ee5f013b6bed45f59f4dfc0d64f4ec866b60f495
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-05-12 13:18:48 -0400

    esercizio base

---
+++ myScript.js
@@ -0,0 +1,19 @@
+const app = new Vue ({
+    el: '#root',
+    data: {
+        emailList: [],
+        cicleTimes: 10
+    },
+
+    mounted() {
+
+        for (i = 0; i < this.cicleTimes; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(resp => {
+                let randomEmail = resp.data.response
+                console.log(randomEmail)
+                this.emailList.push(randomEmail)
+            })
+        } 
+    }
+})
